e370d8bd969544d83a05ff3c6f7bf2c27afd4dc1
Marcel <boersenanalystenapp@gmail.com>
2025-10-11 21:00:35 +0200

    Erste Gegner implementieren
    - Es wird zuerst ein Spielerschiff erstellt und anschließend werden zufällig vom oberen Rand normale Gegner erstellt die sich dann von oben nach unten bewegen bis diese kurz vor dem Schiff stehen bleiben. Die Gegner orientieren sich an der Position des Spielerschiffs das seinen Koordinaten Mittelpunkt in der Mitte hat (Anchor.center).
    - Flame Flutter Package wurde hinzugefügt.

---
+++ lib/features/game_board/presentation/widgets/ships/normal_enemy.dart
@@ -0,0 +1,48 @@
+import 'package:flame/components.dart';
+import 'package:flutter/material.dart';
+import 'package:galaxy_defense/features/game_board/presentation/pages/game_board_page.dart';
+import 'package:galaxy_defense/features/game_board/presentation/widgets/ships/playership.dart';
+
+class NormalEnemy extends PositionComponent with HasGameReference<GalaxyDefenseGame> {
+  static const double normalEnemySize = 30;
+  static const double speed = 150;
+  static const double stopDistance = 80;
+
+  NormalEnemy({super.position}) : super(size: Vector2(normalEnemySize, normalEnemySize), anchor: Anchor.center);
+
+  static final _paint = Paint()
+    ..color = Colors.redAccent
+    ..style = PaintingStyle.fill;
+
+  @override
+  void render(Canvas canvas) {
+    super.render(canvas);
+
+    final path = Path()
+      ..moveTo(size.x / 2, 0)
+      ..lineTo(0, size.y)
+      ..lineTo(size.x, size.y)
+      ..close();
+
+    canvas.drawPath(path, _paint);
+  }
+
+  @override
+  void update(double dt) {
+    super.update(dt);
+
+    final PlayerShip playerShip = game.playerShip;
+
+    final Vector2 toPlayer = playerShip.position - position;
+    final double distanceToPlayer = toPlayer.length;
+
+    if (distanceToPlayer > stopDistance) {
+      // Normalisieren, um Richtungseinheitsvektor zu bekommen
+      final Vector2 direction = toPlayer.normalized();
+      lookAt(playerShip.position);
+      position += direction * (speed * dt);
+    } else {
+      // Hier könnte später z. B. ein Angriff stattfinden
+    }
+  }
+}
